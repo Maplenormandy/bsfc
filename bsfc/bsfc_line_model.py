Fixed a bug in log-likelihood estimation, added synthetic profile tests

bsfc/bsfc_line_model.py
@@ -27,7 +27,7 @@ class LineModel:
     Models a spectra. Uses 2nd order Legendre fitting on background noise,
     and n-th order Hermite on the lines
     """
-    def __init__(self, lam, lamNorm, specBr, sig, lineData, linesFit, hermFuncs):
+    def __init__(self, lam, lamNorm, specBr, sig, whitefield, lineData, linesFit, hermFuncs):
         self.lam = lam
         self.specBr = specBr
         self.sig = sig
@@ -38,6 +38,7 @@ class LineModel:
         self.linesLam = self.lineData.lam[self.linesFit]
         self.linesnames = self.lineData.names[self.linesFit]
         self.linesSqrtMRatio = self.lineData.sqrt_m_ratio[self.linesFit]
+        self.whitefield = whitefield
 
         # Normalized lambda, for evaluating background noise
         self.lamNorm = lamNorm
@@ -357,7 +358,7 @@ class LineModel:
 
         # get chi2 by comparing model prediction and experimental signals
         pred = self.modelPredict(theta)
-        return -np.sum((self.specBr-pred)**2/self.sig**2)
+        return -np.sum((self.specBr-pred)**2/np.abs(pred)*self.whitefield)
 
 
 
@@ -582,6 +583,6 @@ class LineModel:
         theta = [cube[i] for i in range(0, ndim)]
 
         pred = self.modelPredict(theta)
-        ll = -np.sum((self.specBr-pred)**2/self.sig**2)
+        ll = -np.sum((self.specBr-pred)**2/np.abs(pred)*self.whitefield)
 
         return ll

bsfc/bsfc_moment_fitter.py
@@ -187,7 +187,9 @@ class MomentFitter:
         # Indices are [lambda, time, channel]
         self.specBr_all = branchNode.getNode('SPEC:SPECBR').data()
         self.sig_all = branchNode.getNode('SPEC:SIG').data()
+        self.whitefield = self.specBr_all / self.sig_all**2
 
+        """
         if branchB:
             # Hack for now; usually the POS variable is in LYA1 on branch B
             pos_tmp = branchNode.getNode('MOMENTS.LYA1:POS').data()
@@ -196,6 +198,7 @@ class MomentFitter:
             pos_tmp = branchNode.getNode('MOMENTS.'+primary_line.upper()+':POS').data()
 
         self.pos=np.squeeze(pos_tmp[np.where(pos_tmp[:,0]!=-1),:])
+        """
 
         # Maximum number of channels, time bins
         self.maxChan = np.max(branchNode.getNode('BINNING:CHMAP').data())+1
@@ -348,8 +351,9 @@ class MomentFitter:
         lam = self.lam_all[w0:w1,tbin,chbin]
         specBr = self.specBr_all[w0:w1,tbin,chbin]
         sig = self.sig_all[w0:w1,tbin,chbin]
+        whitefield = self.whitefield[w0:w1,tbin,chbin]
 
-        bf = BinFit(lam, specBr, sig, self.lines, range(len(self.lines.names)), n_hermite=n_hermite)
+        bf = BinFit(lam, specBr, sig, whitefield, self.lines, range(len(self.lines.names)), n_hermite=n_hermite)
 
         self.fits[tbin][chbin] = bf
 